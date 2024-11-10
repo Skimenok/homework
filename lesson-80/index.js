567d978f6b1b4efb3b5b47bdae48ba807d99d009
Skimenok <160650633+Skimenok@users.noreply.github.com>
2024-11-10 16:15:02 +0300

    Add files via upload

---
+++ lesson-80/index.js
@@ -0,0 +1,26 @@
+for (let i = 2; i <= 12; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+console.log("задача номер два");
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
+console.log("задача номер три");
+
+let a = 2;
+while (a <= 12) {
+  if (a % 2 == 0) {
+    console.log(a);
+  }
+  a++;
+}
+
+b = 10;
+while (b >= 1) {
+  console.log(b)
+  b--; 
+}
