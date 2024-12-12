e4549150e6a54ad6685a10fc5c51c26f36817303
Skimenok <160650633+Skimenok@users.noreply.github.com>
2024-12-12 14:47:05 +0300

    Add files via upload

--- diplome-5/js/main.js
+++ diplome-5/js/main.js
@@ -1,15 +1,19 @@
 const body = document.querySelector(".body");
 const burger = document.querySelector(".burger");
 const headerTopVerh = document.querySelector(".header__top-verh");
+const button = document.querySelectorAll(".button");
 
-body.addEventListener("click", (even) => {
-  even.preventDefault;
-});
-
+button.forEach((el) => {
+  el.addEventListener('click', even => {
+    even.preventDefault();
+  })
+})
 burger.addEventListener("click", (even) => {
+  even.preventDefault();
   body.classList.toggle("body__opend-modal");
 });
 headerTopVerh.addEventListener("click", (even) => {
+  even.preventDefault();
   if (body.classList.contains("body__opend-modal")) {
     body.classList.remove("body__opend-modal");
   }
@@ -22,10 +26,11 @@ headerTopVerh.addEventListener("click", (even) => {
 //==========================================================================================================//
 
 const hallsTabControls = document.querySelector(".halls__tab__controls");
-// const hallsContents = document.querySelector(".halls__contents");
 
 hallsTabControls.addEventListener("click", (even) => {
+  even.preventDefault();
   if (even.target.classList.contains("halls__tab__controls-link")) {
+    
     document
       .querySelector(".halls__tab__controls-item--opend")
       .classList.remove("halls__tab__controls-item--opend");
