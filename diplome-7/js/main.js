26cb3af2a599f16183ab6b1f0d1daf06acfbcffc
Skimenok <160650633+Skimenok@users.noreply.github.com>
2024-12-15 19:52:17 +0300

    Add files via upload

--- diplome-7/js/main.js
+++ diplome-7/js/main.js
@@ -4,10 +4,10 @@ const headerTopVerh = document.querySelector(".header__top-verh");
 const button = document.querySelectorAll(".button");
 
 button.forEach((el) => {
-  el.addEventListener('click', even => {
+  el.addEventListener("click", (even) => {
     even.preventDefault();
-  })
-})
+  });
+});
 burger.addEventListener("click", (even) => {
   even.preventDefault();
   body.classList.toggle("body__opend-modal");
@@ -30,7 +30,6 @@ const hallsTabControls = document.querySelector(".halls__tab__controls");
 hallsTabControls.addEventListener("click", (even) => {
   even.preventDefault();
   if (even.target.classList.contains("halls__tab__controls-link")) {
-    
     document
       .querySelector(".halls__tab__controls-item--opend")
       .classList.remove("halls__tab__controls-item--opend");
@@ -79,15 +78,22 @@ const swiper = new Swiper(".swiper", {
 //==========================================================================================================//
 //==========================================================================================================//
 
+function syncWidths() {
+  const el1 = document.getElementById("element1");
+  const el2 = document.getElementById("element2");
 
+  if (window.innerWidth < 1300) {
+    el2.style.width = `${el1.offsetWidth}px`; // Устанавливаем ширину эл2 равной ширине эл1
+  } else {
+    el2.style.width = ""; // Сбрасываем ширину эл2, если ширина экрана больше или равна 1300px
+  }
+}
 
+// Инициализация при загрузке страницы
+syncWidths();
 
-
-
-
-
-
-
+// Обработчик события изменения размера окна
+window.addEventListener("resize", syncWidths);
 
 
 //==========================================================================================================//
@@ -95,19 +101,3 @@ const swiper = new Swiper(".swiper", {
 //==========================================================================================================//
 //==========================================================================================================//
 //==========================================================================================================//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
